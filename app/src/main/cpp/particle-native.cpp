317a7e4a6bc4834ac1b804e4477bcefeb7c3c60a
Kaedenn <kaedenn@gmail.com>
2020-02-16 09:00:18 -0900

    Add beginning of a native library

---
+++ app/src/main/cpp/particle-native.cpp
@@ -0,0 +1,5 @@
+//
+// Created by kaedenn on 2/16/20.
+//
+
+#include "particle-native.h"
---
+++ app/src/main/cpp/particle-native.h
@@ -0,0 +1,8 @@
+//
+// Created by kaedenn on 2/16/20.
+//
+
+#ifndef DEBUG_TOY_PARTICLE_NATIVE_H
+#define DEBUG_TOY_PARTICLE_NATIVE_H
+
+#endif //DEBUG_TOY_PARTICLE_NATIVE_H
